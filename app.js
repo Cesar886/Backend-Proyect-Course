88089e4be357ad510c74f0fc4147b46d9774ffd9
Daniel <cesar4herrera@gmail.com>
2024-07-22 10:13:37 -0600

    .env

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
+require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const app = express()
 
 app.use(cors())
 
-const port = 300
+const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-    console.log(`tu app esta lista por http://localhost:${port}`);
+    console.log(`http://localhost:${port}`);
 })
