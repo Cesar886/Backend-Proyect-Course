6b98ec43caf4c7daab791533ed81d0a4c8c3be28
Daniel <cesar4herrera@gmail.com>
2024-07-22 10:40:42 -0600

    .

--- config/mongo.js
+++ config/mongo.js
@@ -1,7 +1,21 @@
-const mogoose = require("mongoose");
+const mongoose = require("mongoose");
 
 const dbConnect = () => {
-
+    const DB_URI = process.env.DB_URI;
+    mongoose.connect(
+        DB_URI, 
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        }, 
+        (err, res) => {
+            if (!err) {
+                console.log(" *** Conexion Exitosa *** ");
+            } else {
+                console.log(" *** Error en la Conexion *** ");
+            }
+        }
+    );
 }
 
-module.exports = dbConnect
+module.exports = dbConnect;
