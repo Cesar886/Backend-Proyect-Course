4c61e43f514d1845b5aae6238ec9917a017064f9
Daniel <cesar4herrera@gmail.com>
2024-07-23 09:54:12 -0600

    const PUBLIC_URL = process.env.PUBLIC_URL;

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,23 +1,25 @@
-const {storageModel} = require("../models/index")
+const { storageModel } = require("../models/index")
+const PUBLIC_URL = process.env.PUBLIC_URL;
 
 const getItems = async (req, res) => {
 
     const data = await storageModel.find({});
 
-    res.send({data})
+    res.send({ data })
 };
-const getItem = (req, res) => {};
+const getItem = (req, res) => { };
 const createItem = async (req, res) => {
     const { body, file } = req
     console.log("🚀 ~ createItem ~ body:", file)
     const fileDate = {
-        filename: file.filename
+        filename: file.filename,
+        url: `${ PUBLIC_URL }/${ file.filename }`
     }
-    // const data = await storageModel.create(body)
-    res.send({file})
+    const data = await storageModel.create(fileDate)
+    res.send({ data })
 };
-const updateItems = (req, res) => {};
-const deleteItems = (req, res) => {};
+const updateItems = (req, res) => { };
+const deleteItems = (req, res) => { };
 
 
 
