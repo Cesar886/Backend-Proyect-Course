6ff9c987d39e27d4d857c8e3007b5ff75b397757
Daniel <cesar4herrera@gmail.com>
2024-07-23 14:19:24 -0600

    error 302 en fileSystem

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,28 +1,62 @@
-const { storageModel } = require("../models/index")
+const { matchedData } = require("express-validator");
+const fs = require("fs");
+const { storageModel } = require("../models/index");
+const { handleHttpError } = require("../utils/handleError");
+
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 const getItems = async (req, res) => {
-
-    const data = await storageModel.find({});
-
-    res.send({ data })
+    try {
+        const data = await storageModel.find({});
+        res.send({ data })
+    } catch (e) {
+        handleHttpError(res, 'ERROR LIST ITEM')
+    }
 };
-const getItem = (req, res) => { };
+
+const getItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req)
+        const data = await storageModel.findById(id);
+        res.send({ data })
+    } catch (e) {
+        handleHttpError(res, 'ERROR GET ITEM')
+    }
+};
+
 const createItem = async (req, res) => {
     const { body, file } = req
     console.log("🚀 ~ createItem ~ body:", file)
     const fileDate = {
         filename: file.filename,
-        url: `${ PUBLIC_URL }/${ file.filename }`
+        url: `${PUBLIC_URL}/${file.filename}`
     }
     const data = await storageModel.create(fileDate)
     res.send({ data })
 };
-const updateItems = (req, res) => { };
-const deleteItems = (req, res) => { };
+
+const updateItem = async (req, res) => { };
+
+const deleteItem = async (req, res) => {
+    try {
+        const { id } = matchedData(req)
+        const dataFile = await storageModel.findById(id);
+        const { filename } = dataFile;
+        const filePath = `${MEDIA_PATH}/${filename}`;
+        fs.unlikSync(filePath)
+        const data = {
+            filePath,
+            deleted: 1
+        }
+        res.send({ data })
+    } catch (e) {
+        handleHttpError(res, 'ERROR DETAILS ITEM')
+    }
+};
 
 
 
 
 
-module.exports = { getItems, createItem, updateItems, deleteItems, getItem };
+module.exports = { getItems, createItem, updateItem, deleteItem, getItem };
--- routes/storage.js
+++ routes/storage.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const uploadMiddleware = require("../utils/handleStorage");
-const { createItem } = require("../controllers/storage");
+const { createItem, getItems, getItem, deleteItem } = require("../controllers/storage");
+const { validatorGetItem } = require("../validators/storage");
 
 const router = express.Router()
 
 // .shift() agarra el primer valor de split()
 // caso contrario .pop() agarra el ultimo valor de split()
 
+
+router.get("/", getItems);
+
+router.get("/:id", validatorGetItem, getItem);
+
+router.delete("/:id", validatorGetItem, deleteItem);
+
 router.post("/", uploadMiddleware.single("myfile"), createItem);
+
 
 module.exports = router; 
