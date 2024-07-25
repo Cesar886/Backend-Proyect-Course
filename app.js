08f9c09c469629d2ed2caf559d6a2f43bde96df9
Daniel <cesar4herrera@gmail.com>
2024-07-25 17:00:26 -0600

    Update npm dependencies

--- app.js
+++ app.js
@@ -2,11 +2,28 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const dbConnect = require('./config/mongo')
+const { IncomingWebhook } = require('@slack/webhook');
+const morganBody = require('morgan-body')
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static("storage"))
+
+const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
+
+const loggerStream = {
+    write: message => {
+    console.log("🚀 ~ Capturando el LOG:", message)
+    }
+}
+morganBody(app), {
+    noColors: true
+    stream: loggerStream,
+    skip: function (req, res) {
+        return res.statusCode < 400
+    }
+}
 
 const port = process.env.PORT || 3000
 
