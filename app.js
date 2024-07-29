19fa19a4951be2ba1d4149a8a35a5899472423e1
Daniel <cesar4herrera@gmail.com>
2024-07-29 10:13:49 -0600

    Remove console.log statements and fix formatting in auth controller

--- app.js
+++ app.js
@@ -2,28 +2,21 @@ require("dotenv").config()
 const express = require("express")
 const cors = require("cors")
 const dbConnect = require('./config/mongo')
-const { IncomingWebhook } = require('@slack/webhook');
 const morganBody = require('morgan-body')
+const loggerStream = require('./utils/handleLogger');
 const app = express()
 
 app.use(cors())
 app.use(express.json())
 app.use(express.static("storage"))
 
-const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
 
-const loggerStream = {
-    write: message => {
-        webHook.send({
-            text: message
-        });
-        console.log("🚀 ~ Capturando el LOG:", message)
-    }
-}
+
+
 morganBody(app, {
     noColors: true,
     stream: loggerStream,
-    skipt: function(req, res) {
+    skipt: function (req, res) {
         return res.statusCode < 400
     }
 });
@@ -33,7 +26,7 @@ const port = process.env.PORT || 3000
 
 // aqui invocamos a las rutas !!
 //TODO localhost/api/____
-app.use("/api",require("./routes"))
+app.use("/api", require("./routes"))
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
---
+++ utils/handleLogger.js
@@ -0,0 +1,15 @@
+const { IncomingWebhook } = require('@slack/webhook');
+const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
+
+
+
+const loggerStream = {
+    write: message => {
+        webHook.send({
+            text: message
+        });
+        console.log("🚀 ~ Capturando el LOG:", message)
+    }
+}
+
+module.exports = loggerStream
